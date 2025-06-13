9cae48058ee9399b671de56f451910d4fc9b5726
Ziad Waleed <ziadwaleedmohamed2003@gmail.com>
2025-06-13 22:09:02 +0300

    fix (job_req_analyst) : fixed the create_task function and removed the unnecessary params

--- app/agents/test.py
+++ app/agents/test.py
@@ -4,15 +4,12 @@ from job_requirement_analyst import JobRequirementAnalyst
 
 job_analyst_agent = JobRequirementAnalyst(
     max_queries=8,
-    search_mode="Platform-specific"  # or "general"
 )
 
 # 2. Create a Crew
 crew = Crew(
     agents=[job_analyst_agent.agent],  # Access the underlying Agent object
-    tasks=[job_analyst_agent.create_task(
-        user_input="I want a .net and C# developer role in Egypt you can use Indeed or Linkedin my experience is 0 years so you must look for fresh rules or juniors!",
-    )],
+    tasks=[job_analyst_agent.create_task()],
     verbose=1
 )
 
@@ -23,12 +20,3 @@ results = crew.kickoff(
                         can use Indeed or Linkedin my experience is 0 years so you must look for fresh rules or juniors!"""
     }
 )
-
-# # 4. Process the results (example)
-# print("Universal Query:", results.universal_query)
-# if results.platform_queries:
-#     print("\nPlatform-Specific Queries:")
-#     for platform, queries in results.platform_queries.items():
-#         print(f"{platform}:")
-#         for query in queries:
-#             print(f" - {query}")
