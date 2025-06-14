db34ab21f6954693701ba45abe3f94bf8020dfbb
Ziad Waleed <ziadwaleedmohamed2003@gmail.com>
2025-06-15 01:12:18 +0300

    IDK what I'm doing rn I got fed up and I keep getting this damn error in the 2nd agent flow I'll try Explicit context instead of implicit

---
+++ app/tools/search_tools.py
@@ -0,0 +1,13 @@
+from crewai.tools import tool
+from app.clients import get_search_client
+
+
+@tool
+def tavily_search_engine_tool(query: str):
+    """Useful for search-based queries. Use this to find current information about any query related pages using a search engine"""
+
+    tavliy_search_client = get_search_client()
+    return tavliy_search_client.search(query)
+
+
+# TODO : Implement Google Serper Search tool (IDK why TF i did this to myself)
